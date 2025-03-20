9b2e5203ebda48d97c4c57be8ea3158579d29aa7
Lucas Rios <lucasriosdev@gmail.com>
2025-03-20 00:08:59 -0300

    Criação da tela de Cadastro de Usuários.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,55 @@
-import { Title, Container } from "./styles";
+import {
+  Title,
+  Container,
+  TopBackground,
+  Form,
+  ContainerInputs,
+  Input,
+  Button,
+  InputLabel,
+} from "./styles";
 
-function App() {
-  
-
+function Home() {
   return (
     <Container>
-      <Title>Ok, React.</Title>
+      <TopBackground>
+        <div>
+          <img />
+        </div>
+      </TopBackground>
+
+      <Form>
+        <Title>Cadastrar Usuário</Title>
+
+        <ContainerInputs>
+          <div>
+            <div>
+              <InputLabel>
+                Nome<span>*</span>
+              </InputLabel>
+              <Input type="text" placeholder="Nome do Usuário" />
+            </div>
+
+            <div>
+              <InputLabel>
+                Idade<span>*</span>
+              </InputLabel>
+              <Input type="number" placeholder="Idade do Usuário" />
+            </div>
+
+            <div>
+              <InputLabel>
+                E-mail<span>*</span>
+              </InputLabel>
+              <Input type="email" placeholder="E-mail do Usuário" />
+            </div>
+          </div>
+        </ContainerInputs>
+
+        <Button>Cadastrar Usuário</Button>
+      </Form>
     </Container>
   );
 }
 
-export default App;
+export default Home;
