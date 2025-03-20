f5cd9792f6b88e682cad91edd87674e8048f50e9
Lucas Rios <lucasriosdev@gmail.com>
2025-03-20 17:54:17 -0300

    Alteração no index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,4 @@
-
-import {useRef} from 'react'
+import { useRef } from "react";
 import api from "../../services/api";
 
 import {
@@ -16,22 +15,18 @@ import {
 import UsersImage from "../../assets/users.png";
 
 function Home() {
+  const inputName = useRef();
+  const inputAge = useRef();
+  const inputEmail = useRef();
 
-  const inputName = useRef()
-  const inputAge = useRef()
-  const inputEmail = useRef()
-
-   async function registerNewUser(){
-     const data = await api.post('/usuarios', {
+  async function registerNewUser() {
+    await api.post("/usuarios", {
       email: inputEmail.current.value,
-      age : parseInt(inputAge.current.value),
-      name: inputName.current.value
-    })
-
-    console.log(data)
+      age: parseInt(inputAge.current.value),
+      name: inputName.current.value,
+    });
   }
 
-  
   return (
     <Container>
       <TopBackground>
@@ -42,31 +37,39 @@ function Home() {
         <Title>Cadastrar Usuário</Title>
 
         <ContainerInputs>
-          
-            <div>
-              <InputLabel>
-                Nome<span> *</span>
-              </InputLabel>
-              <Input type="text" placeholder="Nome do Usuário"  ref={inputName}/>
-            </div>
+          <div>
+            <InputLabel>
+              Nome<span> *</span>
+            </InputLabel>
+            <Input type="text" placeholder="Nome do Usuário" ref={inputName} />
+          </div>
 
-            <div>
-              <InputLabel>
-                Idade<span> *</span>
-              </InputLabel>
-              <Input type="number" placeholder="Idade do Usuário" ref={inputAge} />
-            </div>
-          
+          <div>
+            <InputLabel>
+              Idade<span> *</span>
+            </InputLabel>
+            <Input
+              type="number"
+              placeholder="Idade do Usuário"
+              ref={inputAge}
+            />
+          </div>
         </ContainerInputs>
 
         <div style={{ width: "100%" }}>
           <InputLabel>
             E-mail<span> *</span>
           </InputLabel>
-          <Input type="email" placeholder="E-mail do Usuário"  ref={inputEmail}/>
+          <Input
+            type="email"
+            placeholder="E-mail do Usuário"
+            ref={inputEmail}
+          />
         </div>
 
-        <Button type='button' onClick={registerNewUser} >Cadastrar Usuário</Button>
+        <Button type="button" onClick={registerNewUser}>
+          Cadastrar Usuário
+        </Button>
       </Form>
     </Container>
   );
