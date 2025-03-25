116d5b2c7b1ca5390be4cb41bad41f7d66801724
Lucas Rios <lucasriosdev@gmail.com>
2025-03-25 15:02:21 -0300

    Utilizando o Proptypes no projeto

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,8 +1,13 @@
+import PropTypes from "prop-types";
+
 import { Button } from "./styles";
 
 function DefaultButton({ children, ...props }) {
-  console.log(props);
   return <Button {...props}>{children}</Button>;
 }
+
+DefaultButton.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default DefaultButton;
