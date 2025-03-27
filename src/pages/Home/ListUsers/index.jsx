3b338f2b5ed0a2099827b31295d741d1fb0b1da2
Lucas Rios <lucasriosdev@gmail.com>
2025-03-27 13:37:58 -0300

    estilizando a tela de usuarios

--- src/pages/Home/ListUsers/index.jsx
+++ src/pages/Home/ListUsers/index.jsx
@@ -1,10 +1,15 @@
 import { useEffect, useState } from "react";
 import api from "../../../services/api";
 import TopBackground from "../../../components/TopBackground";
-import { Button } from "../styles";
+import Button from "../../../components/Button";
+import Trash from "../../../assets/trash.svg";
+import { useNavigate } from "react-router-dom";
+
+import { Container, ContainerUsers, CardUsers, TrashIcon, Title, AvatarUser } from "./styles";
 
 function ListUsers() {
   const [users, setUsers] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function getUsers() {
@@ -16,20 +21,28 @@ function ListUsers() {
   }, []);
 
   return (
-    <div>
+    <Container>
       <TopBackground />
-      <h1>Listagem de Usuários</h1>
+      <Title>Lista de Usuários</Title>
 
-      {users.map((user) => (
-        <div key={user.id}>
-          <p>{user.name}</p>
-          <p>{user.email}</p>
-          <p>{user.age}</p>
-        </div>
-      ))}
+      <ContainerUsers>
+        {users.map((user) => (
+          <CardUsers key={user.id}>
+            <AvatarUser src={`https://avatar.iran.liara.run/public?username=${user.id}`} />
+            <div>
+              <h3>{user.name}</h3>
+              <p>{user.email}</p>
+              <p>{user.age}</p>
+            </div>
+            <TrashIcon src={Trash} alt="icone-lixo" />
+          </CardUsers>
+        ))}
+      </ContainerUsers>
 
-      <Button>Voltar</Button>
-    </div>
+      <Button type="button" onClick={() => navigate("/cadastro-de-usuarios")}>
+        Voltar
+      </Button>
+    </Container>
   );
 }
 
