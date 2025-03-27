73b0c995268286ce7c2645a8c0988e1863a04d58
Lucas Rios <lucasriosdev@gmail.com>
2025-03-27 11:53:44 -0300

    useState

--- src/pages/Home/ListUsers/index.jsx
+++ src/pages/Home/ListUsers/index.jsx
@@ -1,13 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import api from "../../../services/api";
 import TopBackground from "../../../components/TopBackground";
 import { Button } from "../styles";
 
 function ListUsers() {
+  const [users, setUsers] = useState([]);
+
   useEffect(() => {
     async function getUsers() {
-      const usersFromApi = await api.get("/usuarios");
-      console.log(usersFromApi);
+      const { data } = await api.get("/usuarios");
+
+      setUsers(data);
     }
     getUsers();
   }, []);
@@ -16,6 +19,15 @@ function ListUsers() {
     <div>
       <TopBackground />
       <h1>Listagem de Usuários</h1>
+
+      {users.map((user) => (
+        <div>
+          <p>{user.name}</p>
+          <p>{user.email}</p>
+          <p>{user.age}</p>
+        </div>
+      ))}
+
       <Button>Voltar</Button>
     </div>
   );
